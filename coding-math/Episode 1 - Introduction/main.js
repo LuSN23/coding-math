231a902020b34147f6beb062c3b17fdf4e294155
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-11 01:44:40 -0300

    Drawing random lines

--- coding-math/Episode 1 - Introduction/main.js
+++ coding-math/Episode 1 - Introduction/main.js
@@ -4,5 +4,10 @@ window.onload = function() {
     var width = canvas.width = window.innerWidth;
     var height = canvas.height = window.innerHeight;
 
-    context.fillRect(0, 0, width, height);
+    for(var i = 0; i < 100; i += 1) {
+        context.beginPath();
+        context.moveTo(Math.random() * width, Math.random() * height);
+        context.lineTo(Math.random() * width, Math.random() * height);
+        context.stroke();
+    }
 }
