239f888f369df7b5f840c9e49b4ae8b35eccf842
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-12 01:07:02 -0300

    Creating basic trig01.js file

---
+++ coding-math/Episode 2, 3 - Trigonometry/trig01.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var canvas = document. getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var width = canvas.width = window.innerWidth;
+    var height = canvas.height = window.innerHeight;
+};
