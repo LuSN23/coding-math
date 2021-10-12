f140f97cd8f140dfe4ea5a617f67131e254126fc
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-12 01:19:18 -0300

    Printing the values of sin in the console

--- coding-math/Episode 2, 3 - Trigonometry/trig01.js
+++ coding-math/Episode 2, 3 - Trigonometry/trig01.js
@@ -3,4 +3,8 @@ window.onload = function() {
     var context = canvas.getContext("2d");
     var width = canvas.width = window.innerWidth;
     var height = canvas.height = window.innerHeight;
+
+    for(var angle = 0; angle < Math.PI * 2; angle += .01) {
+        console.log(Math.sin(angle));
+    }
 };
