3e9d5c11a4b950d08fadc8b76591e02f48c9591e
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-30 17:33:38 -0300

    Adjusting the sine wave and fixing the name of the folder of Episode 2

--- coding-math/Episode 2 - Intro to Trigonometry/trig01.js
+++ coding-math/Episode 2 - Intro to Trigonometry/trig01.js
@@ -4,8 +4,8 @@ window.onload = function() {
     var width = canvas.width = window.innerWidth;    //The width is equal to the width of the inner of the object window
     var height = canvas.height = window.innerHeight;  //The height is equal to the height of the inner of the object window
 
-    
-    
+    context.translate(0, height / 2); //Adjusting with translate the positions of x and y, to show the sine wave at the center of the screen
+    context.scale(1, -1); //Adjusting with scale, resizing the element 2D to revert the axis of the waves considering the monitors reverted positions
 
     for(var angle = 0; angle < Math.PI * 2; angle += .01) { 
        var x = angle * 200;             //Spacing individual data points, using x and y axis
