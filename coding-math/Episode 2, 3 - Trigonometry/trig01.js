66e06ec66a7b4aee89c219dbafed62c823acec40
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2021-10-30 17:20:24 -0300

    Drawing the sine wave on the screen

--- coding-math/Episode 2, 3 - Trigonometry/trig01.js
+++ coding-math/Episode 2, 3 - Trigonometry/trig01.js
@@ -1,10 +1,16 @@
 window.onload = function() {
-    var canvas = document. getElementById("canvas");
-    var context = canvas.getContext("2d");
-    var width = canvas.width = window.innerWidth;
-    var height = canvas.height = window.innerHeight;
+    var canvas = document. getElementById("canvas"); //Pick the canvas element
+    var context = canvas.getContext("2d");           //Define a context to canvas
+    var width = canvas.width = window.innerWidth;    //The width is equal to the width of the inner of the object window
+    var height = canvas.height = window.innerHeight;  //The height is equal to the height of the inner of the object window
 
-    for(var angle = 0; angle < Math.PI * 2; angle += .01) {
-        console.log(Math.sin(angle));
+    
+    
+
+    for(var angle = 0; angle < Math.PI * 2; angle += .01) { 
+       var x = angle * 200;             //Spacing individual data points, using x and y axis
+       var y = Math.sin(angle) * 200;
+
+       context.fillRect(x, y, 5, 5); //Draws a rectangle
     }
 };
